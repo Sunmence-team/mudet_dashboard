f37d53079e28b19fa008e76cfe910c41dd63511e
Bamitale Abdulazeem Irebami <youngbeeh08@gmail.com>
2025-10-09 03:37:02 +0100

    Zenith (#22)
    * login
    
    * login
    
    * on the amendment users profile and the admin users, transactions
    
    * created the package page for admin and integrate api (CRUD) in it
    
    * product
    
    * not done
    
    * on the all products
    
    * almost
    
    * designed the transfer funds page
    
    * lol
    
    * pull from branch pentium
    
    * finish the upgrade plan and transfer fund api
    
    * get all product and get plans
    
    * let's go
    
    * edit wallet transfer to be from e_wallet to purchased wallet
    
    * reg done
    
    * user stokist
    
    * updated the e-wallet transfer workflow
    
    * pulled recent main
    
    * edited and integrate other api endpoints
    
    ---------
    
    Co-authored-by: Sanni Mubarak <oluwatoyinayomide6@gmail.com>
    Co-authored-by: Abdulmuheez <abdulmuizayomide2@gmail.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,10 @@ import { IoCloseOutline } from "react-icons/io5";
 import { IoIosLogOut } from "react-icons/io";
 import { useEffect } from "react";
 import { useUser } from "../context/UserContext";
-import { MdKeyboardDoubleArrowLeft, MdKeyboardDoubleArrowRight } from "react-icons/md";
+import {
+  MdKeyboardDoubleArrowLeft,
+  MdKeyboardDoubleArrowRight,
+} from "react-icons/md";
 import { useCart } from "../context/CartProvider";
 
 const Navbar = () => {
@@ -16,7 +19,6 @@ const Navbar = () => {
   const { cart } = useCart();
   const [isOpen, setIsOpen] = useState(false);
   const cartItem = cart.length;
-
 
   const scrollRef = useRef(null);
   const [canScrollLeft, setCanScrollLeft] = useState(false);
@@ -167,7 +169,10 @@ const Navbar = () => {
     },
   ];
 
-  const filteredLinks = navItems.filter(navItem => (Array.isArray(navItem.role) && navItem.role.includes(user?.role)));
+  const filteredLinks = navItems.filter(
+    (navItem) =>
+      Array.isArray(navItem.role) && navItem.role.includes(user?.role)
+  );
   const userName = `${user?.first_name} ${user?.last_name}`;
 
   return (
@@ -191,21 +196,21 @@ const Navbar = () => {
           {/* Scrollable links section */}
           <div className="relative flex items-center overflow-hidden w-3/4">
             {/* Left scroll */}
-            {
-              canScrollLeft && (
-                <button
-                  onClick={scrollLeft}
-                  className="absolute left-0 z-10 bg-tetiary text-primary w-8 h-8 flex items-center justify-center rounded-full shadow-md hover:scale-105 transition"
-                >
-                  <MdKeyboardDoubleArrowLeft />
-                </button>
-              )
-            }
+            {canScrollLeft && (
+              <button
+                onClick={scrollLeft}
+                className="absolute left-0 z-10 bg-tetiary text-primary w-8 h-8 flex items-center justify-center rounded-full shadow-md hover:scale-105 transition"
+              >
+                <MdKeyboardDoubleArrowLeft />
+              </button>
+            )}
 
             {/* Links */}
             <ul
               ref={scrollRef}
-              className={`md:flex hidden flex-nowrap items-center gap-4 overflow-x-scroll no-scrollbar scroll-smooth flex-1 ${canScrollLeft ? "ps-10" : ""} pe-10`}
+              className={`md:flex hidden flex-nowrap items-center gap-4 overflow-x-scroll no-scrollbar scroll-smooth flex-1 ${
+                canScrollLeft ? "ps-10" : ""
+              } pe-10`}
             >
               {filteredLinks.map(({ name, path }, index) => (
                 <NavLink
@@ -213,7 +218,9 @@ const Navbar = () => {
                   key={index}
                   className={({ isActive }) =>
                     `nav-links relative whitespace-nowrap text-black cursor-pointer py-1 ${
-                      isActive ? "active text-primary !font-extrabold text-base" : "font-medium text-sm"
+                      isActive
+                        ? "active text-primary !font-extrabold text-base"
+                        : "font-medium text-sm"
                     }`
                   }
                   onClick={() => setIsOpen(false)}
@@ -224,16 +231,14 @@ const Navbar = () => {
             </ul>
 
             {/* Right scroll */}
-            {
-              canScrollRight && (
-                <button
-                  onClick={scrollRight}
-                  className="absolute right-0 z-10 bg-tetiary text-primary w-8 h-8 flex items-center justify-center rounded-full shadow-md hover:scale-105 transition"
-                >
-                  <MdKeyboardDoubleArrowRight />
-                </button>
-              )
-            }
+            {canScrollRight && (
+              <button
+                onClick={scrollRight}
+                className="absolute right-0 z-10 bg-tetiary text-primary w-8 h-8 flex items-center justify-center rounded-full shadow-md hover:scale-105 transition"
+              >
+                <MdKeyboardDoubleArrowRight />
+              </button>
+            )}
           </div>
         </div>
 
@@ -245,9 +250,11 @@ const Navbar = () => {
             to={"/user/cart"}
             className="relative w-10 h-10 flex justify-center items-center rounded-full font-bold text-xl bg-tetiary text-primary"
           >
-            <p className="absolute bottom-[1.4rem] left-[2rem] text-base">
-              {cartItem !== 0 ? cartItem : " "}
-            </p>
+            {cartItem !== 0 && (
+              <p className="absolute bottom-[1.4rem] left-[1.4rem] text-sm bg-primary text-[#EFF7F0] px-[7px] py-[1px] rounded-full">
+                {cartItem !== 0 ? cartItem : " "}
+              </p>
+            )}
             <IoMdCart />
           </Link>
           <Link
@@ -267,8 +274,9 @@ const Navbar = () => {
       </nav>
 
       <nav
-        className={`fixed top-0 left-0 z-999 w-full h-screen bg-tetiary flex flex-col items-center justify-between gap-6 px-4 py-6 shadow-md ${isOpen ? "slide-in" : "slide-out"
-          }`}
+        className={`fixed top-0 left-0 z-999 w-full h-screen bg-tetiary flex flex-col items-center justify-between gap-6 px-4 py-6 shadow-md ${
+          isOpen ? "slide-in" : "slide-out"
+        }`}
       >
         <div className="flex flex-col h-[calc(100%-40px-24px)] w-full md:gap-6 gap-3">
           <div className="flex flex-row-reverse items-center justify-between ">
@@ -294,10 +302,11 @@ const Navbar = () => {
                   key={index}
                   className={({ isActive }) => `
                                         nav-links relative font-medium whitespace-nowrap text-black cursor-pointer text-base py-1
-                                        ${isActive
-                      ? "active text-primary !font-extrabold"
-                      : ""
-                    }
+                                        ${
+                                          isActive
+                                            ? "active text-primary !font-extrabold"
+                                            : ""
+                                        }
                                     `}
                   onClick={() => setIsOpen(false)}
                 >
--- src/components/modals/ProductModal.jsx
+++ src/components/modals/ProductModal.jsx
@@ -1,9 +1,21 @@
 import { ShoppingCart } from "lucide-react";
 import React from "react";
 
-const ProductModal = ({ product, closeAction, onAddToCart,onRemoveFromCart, productQuantity }) => {
-  const { product_name, imageSrc, product_description, product_pv, price } = product;
-
+const ProductModal = ({
+  product,
+  closeAction,
+  onAddToCart,
+  onRemoveFromCart,
+  productQuantity,
+}) => {
+  const IMAGE_URL = import.meta.env.VITE_IMAGE_BASE_URL;
+  const {
+    product_name,
+    product_image,
+    product_description,
+    product_pv,
+    price,
+  } = product;
   return (
     <div className="fixed inset-0 backdrop-blur-xs bg-black/80 bg-opacity-50 flex items-center justify-center z-50 p-4">
       <div className="relative bg-white rounded-xl shadow-xl max-w-2xl w-full max-h-[90vh] overflow-y-auto">
@@ -16,7 +28,7 @@ const ProductModal = ({ product, closeAction, onAddToCart,onRemoveFromCart, prod
         <div className="p-6 flex lg:flex-row items-center flex-col gap-6">
           <div className="w-full lg:w-1/3 lg:h-1/2 h-[200px]">
             <img
-              src={imageSrc}
+              src={`${IMAGE_URL}/${product_image}`}
               alt={product_name}
               className="w-full h-full object-contain bg-gray-100 rounded-md"
             />
@@ -32,7 +44,8 @@ const ProductModal = ({ product, closeAction, onAddToCart,onRemoveFromCart, prod
               {product_description}
             </p>
             <div className="text-sm font-bold text-black mb-6">
-              Product Point Value: <span className="text-primary">{product_pv}PV</span>
+              Product Point Value:{" "}
+              <span className="text-primary">{product_pv}PV</span>
             </div>
             <div className="flex justify-between items-center gap-1">
               <button
